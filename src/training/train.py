bb459173331b06e63a11f6affbb508d20c32a7b5
ogzincdlp <o.incedalip@gmail.com>
2025-12-28 19:15:25 +0300

    add training methods

---
+++ src/training/train.py
@@ -0,0 +1,49 @@
+from transformers import TrainingArguments, Trainer
+import evaluate
+import numpy as np
+
+from src.config.config_helper import ConfigHelper
+from src.data.load_dataset import HuggingFaceDataset
+from src.modeling.tokenizer import DataTokenizer
+from src.modeling.collator import Collator
+from src.modeling.modeling import Modeling
+
+class Training():
+    def __init__(self):
+        config_helper = ConfigHelper()
+        self.config = config_helper.get_config()
+
+    def train(self):
+        dataset = HuggingFaceDataset()
+        tokenizer = DataTokenizer(dataset)
+        collator = Collator(tokenizer=tokenizer)
+        model = Modeling()
+        training_args = self._get_training_args()
+
+        trainer = Trainer(
+            model.model,
+            training_args,
+            train_dataset=tokenizer.tokenized_datasets["train"],
+            eval_dataset=tokenizer.tokenized_datasets["validation"],
+            data_collator=collator.collator,
+            processing_class=tokenizer.tokenizer,
+            compute_metrics=Training.compute_metrics,
+        )
+
+
+    def _get_training_args(self):
+        output_model_name = self.config['training']['output_model_name']
+        training_args = TrainingArguments(output_model_name,
+            eval_strategy="epoch",
+            save_strategy="epoch",
+            learning_rate=2e-5,
+            num_train_epochs=3,
+            push_to_hub=True)
+        return training_args
+    
+    @staticmethod
+    def compute_metrics(eval_preds):
+        metric = evaluate.load("glue", "mrpc")
+        logits, labels = eval_preds
+        predictions = np.argmax(logits, axis=-1)
+        return metric.compute(predictions=predictions, references=labels)
